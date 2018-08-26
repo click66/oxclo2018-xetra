2d285319974bc26b097656a008470938572c6e00
Clark Sirl <clark.sirl@efficioconsulting.com>
2018-08-26 16:29:13 +0100

    Removed xml reporting; added sys.exit to get exit code

--- tests/run.py
+++ tests/run.py
@@ -6,4 +6,4 @@ rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 sys.path.insert(0, os.path.abspath(os.path.join(rootdir, "xetra_analyser")))
 
-pytest.main([os.path.join(rootdir, "tests"), "--junitxml", os.path.join(rootdir, "test_results.xml")])
+sys.exit(pytest.main([os.path.join(rootdir, "tests")]))
